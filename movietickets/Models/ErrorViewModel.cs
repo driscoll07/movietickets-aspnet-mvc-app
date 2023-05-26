cbf1b99479fd6b1a0b800a3c3e2d08e84f986d9b
user <dkshatree88@gmail.com>
2023-05-26 15:36:05 +0545

    Add project files.

---
+++ movietickets/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace movietickets.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
